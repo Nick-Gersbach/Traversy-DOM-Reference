6c94f65a7a3aa8e761ae17a67686e07a0e180235
Nicholas Gersbach <nicholas.j.gersbach@gmail.com>
2020-12-13 17:51:33 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let val;
+
+//Shows everything in our current document
+val = document;
+
+//Show all elements in our document individually
+val = document.all;
+
+//Show an element in our document by number (0 based) like with arrays
+val = document.all[3];
+
+//Shows us the totoal number of elements on a page
+val = document.all.length;
+
+//Show all elements in the head
+val = document.head;
+
+//Show all elements in the body
+val = document.body;
+
+//Show the doctype for the webpage (HTML5 in this case = <!DocTYPE html>)
+val = document.doctype;
+
+//Show the domain ping (IP) you're on
+val = document.domain;
+
+//Show the web adddress you're at
+val = document.URL;
+
+//Show the charset the web page is using (UTF-8)
+val = document.characterSet;
+
+//Show the content type (text/html)
+val = document.contentType;
+
+
+
+
+
+console.log(val);
